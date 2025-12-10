1993c7d1df92ec3a635560fa5af9477fc10e6f61
Star_Wasp7272 <star.wasp7272@gmail.com>
2025-12-10 17:30:24 +0100

    add about page and footer

--- main.py
+++ main.py
@@ -2,17 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/music")
 def music():
     return render_template("music.html")
 
+
 @app.route("/movies")
 def movies():
     return render_template("movies.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
